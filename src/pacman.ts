830b10c491d3a83be807d364dd12c1eb095536be
dennis <denn98987@gmail.com>
2021-07-20 19:14:33 +0500

    fix: pick out constant = 20

--- src/pacman.ts
+++ src/pacman.ts
@@ -15,13 +15,15 @@ export default class Pacman implements IAgent {
     y: number;
 
     private _direction: DIR;
-    private _image: HTMLImageElement;
+    private readonly _image: HTMLImageElement;
     private _ctx: CanvasRenderingContext2D;
-    constructor(x: number, y: number, dir: DIR, ctx: CanvasRenderingContext2D) {
+    private _size: number;
+    constructor(x: number, y: number, dir: DIR, ctx: CanvasRenderingContext2D, size: number = 20) {
         this.x = x;
         this.y = y;
         this._direction = dir;
         this._ctx = ctx;
+        this._size = size;
         this._image = new Image();
         this._image.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Pacman.svg/813px-Pacman.svg.png";
     }
@@ -43,6 +45,6 @@ export default class Pacman implements IAgent {
     }
 
     public draw(): void {
-        this._ctx.drawImage(this._image, this.x*20, this.y*20, 20, 20);
+        this._ctx.drawImage(this._image, this.x*this._size, this.y*this._size, this._size, this._size);
     }
 }
