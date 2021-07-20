7506c622a3dd131c67b4624d9ce564ca64d6a271
dennis <denn98987@gmail.com>
2021-07-20 19:45:45 +0500

    feat: eat food and check

--- src/pacman.ts
+++ src/pacman.ts
@@ -1,4 +1,6 @@
 import IAgent from "./IAgent.js";
+import Game from "./game";
+import Cell, {CELLTYPE} from "./cell";
 
 export enum DIR {
     Up,
@@ -17,34 +19,57 @@ export default class Pacman implements IAgent {
     private _direction: DIR;
     private readonly _image: HTMLImageElement;
     private _ctx: CanvasRenderingContext2D;
-    private _size: number;
-    constructor(x: number, y: number, dir: DIR, ctx: CanvasRenderingContext2D, size: number = 20) {
+    private readonly _size: number;
+    private readonly _game: Game;
+    constructor(x: number, y: number, dir: DIR, ctx: CanvasRenderingContext2D, game: Game, size: number = 20) {
         this.x = x;
         this.y = y;
         this._direction = dir;
         this._ctx = ctx;
         this._size = size;
+        this._game = game;
         this._image = new Image();
         this._image.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Pacman.svg/813px-Pacman.svg.png";
     }
 
-    public move(): void {
+    public move(): Cell {
+        let cell = this._game.cellArray[this.x][this.y];
         switch (this._direction){
             case DIR.Up:
-                this.y += 1;
+                if(this._game.cellArray[this.x][this.y+1].type !== CELLTYPE.Wall)
+                    if(this._game.cellArray[this.x][this.y+1].type === CELLTYPE.Food)
+                        this.eatFood(this.x, this.y+1);
+                    this.y += 1;
                 break;
             case DIR.Down:
-                this.y -= 1;
+                if(this._game.cellArray[this.x][this.y-1].type !== CELLTYPE.Wall)
+                    if(this._game.cellArray[this.x][this.y-1].type === CELLTYPE.Food)
+                        this.eatFood(this.x, this.y-1);
+                    this.y -= 1;
                 break;
             case DIR.Left:
-                this.x -= 1;
+                if(this._game.cellArray[this.x-1][this.y].type !== CELLTYPE.Wall)
+                    if(this._game.cellArray[this.x-1][this.y].type === CELLTYPE.Food)
+                        this.eatFood(this.x-1, this.y);
+                    this.x -= 1;
                 break;
             case DIR.Right:
-                this.x += 1;
+                if(this._game.cellArray[this.x+1][this.y].type !== CELLTYPE.Wall)
+                    if(this._game.cellArray[this.x+1][this.y].type === CELLTYPE.Food)
+                        this.eatFood(this.x+1, this.y);
+                    this.x += 1;
+            default:
+                break;
         }
+
+        return cell;
     }
 
     public draw(): void {
         this._ctx.drawImage(this._image, this.x*this._size, this.y*this._size, this._size, this._size);
+    }
+
+    public eatFood(x: number, y: number): void {
+        this._game.cellArray[x][y].type = CELLTYPE.Empty;
     }
 }
