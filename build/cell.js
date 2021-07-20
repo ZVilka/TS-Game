90157aba069d0674f263dfb6bf44919d9411aad7
ZVika <zaitsewavika@yandex.ru>
2021-07-20 12:45:40 +0500

    creating class cell

--- build/cell.js
+++ build/cell.js
@@ -14,5 +14,44 @@ export default class Cell {
         this.context = context;
     }
     draw() {
+        switch (this.type) {
+            case CELLTYPE.Empty: {
+                this._drawRectangle("MediumBlue");
+                break;
+            }
+            case CELLTYPE.Food: {
+                this._drawRectangle("MediumBlue");
+                this._drawCircle('white');
+                break;
+            }
+            case CELLTYPE.Wall: {
+                this._drawRectangle("MediumBlue");
+                this._drawImage('assets/img/wall.svg', 100);
+                break;
+            }
+            default: {
+                return;
+            }
+        }
+    }
+    _drawRectangle(color) {
+        this.context.fillStyle = color;
+        this.context.fillRect(this.x, this.y, 100, 100);
+    }
+    _drawCircle(color) {
+        this.context.beginPath();
+        this.context.arc(this.x + 50, this.y + 50, 25, 0, 2 * Math.PI, false);
+        this.context.fillStyle = color;
+        this.context.fill();
+        this.context.lineWidth = 1;
+        this.context.strokeStyle = color;
+        this.context.stroke();
+    }
+    _drawImage(url, size) {
+        let img = new Image();
+        img.src = url;
+        img.onload = () => {
+            this.context.drawImage(img, this.x, this.y, size, size);
+        };
     }
 }
--- src/cell.ts
+++ src/cell.ts
@@ -9,19 +9,64 @@ export default class Cell {
     y: number;
     type: CELLTYPE;
     weight: number = 0;
+    drawSize: number;
 
     neighborsArray: Cell[] = [];
 
     context: CanvasRenderingContext2D;
 
-    constructor(x: number, y: number, type: CELLTYPE, context: CanvasRenderingContext2D) {
+    constructor(x: number, y: number, type: CELLTYPE, context: CanvasRenderingContext2D, sizeCell = 10) {
         this.x = x;
         this.y = y;
         this.type = type;
         this.context = context;
+        this.drawSize = sizeCell;
     }
 
     public draw(): void {
+        switch (this.type) {
+            case CELLTYPE.Empty: {
+                this._drawRectangle("MediumBlue");
+                break;
+            }
+            case CELLTYPE.Food: {
+                this._drawRectangle("MediumBlue");
+                this._drawCircle('white');
+                break;
+            }
+            case CELLTYPE.Wall: {
+                this._drawRectangle("MediumBlue");
+                this._drawImage('assets/img/wall.svg', this.drawSize);
+                break;
+            }
+            default: {
+                return;
+            }
+        }
+    }
 
+    protected _drawRectangle(color: string) {
+        this.context.fillStyle = color;
+        this.context.fillRect(this.x, this.y, this.drawSize, this.drawSize);
+    }
+
+    protected _drawCircle(color: string) {
+        const halfOfCellSize = this.drawSize / 2;
+        this.context.beginPath();
+        this.context.arc(this.x + halfOfCellSize, this.y + halfOfCellSize, 25,
+            0, 2 * Math.PI, false);
+        this.context.fillStyle = color;
+        this.context.fill();
+        this.context.lineWidth = 1;
+        this.context.strokeStyle = color;
+        this.context.stroke();
+    }
+
+    protected _drawImage(url: string, size: number) {
+        let img = new Image();
+        img.src = url;
+        img.onload = () => {
+            this.context.drawImage(img, this.x, this.y, size, size);
+        }
     }
 }
